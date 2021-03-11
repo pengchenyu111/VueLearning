0369053056b155c2b069be2e38f429e07bf27a71
pengchenyu <1106998960@qq.com>
2021-03-11 22:43:32 +0800

    VueRouter——重定向，实现默认到首页的效果

--- learnVueRouter/src/router/index.js
+++ learnVueRouter/src/router/index.js
@@ -6,22 +6,24 @@ import About from '../components/About'
 // 1.通过Vue.use(插件), 安装插件
 Vue.use(Router)
 
+const routes =  [
+  {
+    // 重定向，实现默认到首页的效果
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/about',
+    component: About
+  }
+]
+
 // 2.创建VueRouter对象
 // 3.将router对象传入到Vue实例
 export default new Router({
-  routes: [
-    {
-      // 重定向，实现默认到首页的效果
-      path: '',
-      redirect: Home
-    },
-    {
-      path: '/home',
-      component: Home
-    },
-    {
-      path: '/about',
-      component: About
-    }
-  ]
+  routes
 })
