120d1a5dad4703fb53efa3aaf9cccedc123700c3
pengchenyu <1106998960@qq.com>
2021-03-11 22:31:10 +0800

    VueRouter——使用router-link和router-view标签切换页面

--- learnVueRouter/src/App.vue
+++ learnVueRouter/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h2>网站标题</h2>
+    <router-link to="/home">首页</router-link>
+    <router-link to="/about">关于</router-link>
+    <router-view></router-view>
+    <h2>版权信息</h2>
   </div>
 </template>
 
@@ -12,12 +15,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- learnVueRouter/src/router/index.js
+++ learnVueRouter/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../components/Home'
+import About from '../components/About'
 
+// 1.通过Vue.use(插件), 安装插件
 Vue.use(Router)
 
+// 2.创建VueRouter对象
+// 3.将router对象传入到Vue实例
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/about',
+      component: About
     }
   ]
 })
