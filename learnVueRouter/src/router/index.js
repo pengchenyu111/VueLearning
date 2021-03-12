aebda8ee27e63d1c64855c4d931054a65e71fe76
pengchenyu <1106998960@qq.com>
2021-03-12 21:38:56 +0800

    VueRouter——路由懒加载

--- learnVueRouter/src/router/index.js
+++ learnVueRouter/src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from '../components/Home'
-import About from '../components/About'
-import User from '../components/User'
+// import Home from '../components/Home'
+// import About from '../components/About'
+// import User from '../components/User'
+
+// 懒加载方式
+// 路由懒加载的主要作用就是将路由对应的组件打包成一个个的js代码块，只有在这个路由被访问到的时候, 才加载对应的组件，这样就更加高效了
+const Home = () => import('../components/Home')
+const About = () => import('../components/About')
+const User = () => import('../components/User')
 
 // 1.通过Vue.use(插件), 安装插件
 Vue.use(Router)
