6578100bcf0af1a89ecafe94a7d5fdd3736a0567
pengchenyu <1106998960@qq.com>
2021-03-15 20:58:06 +0800

    Vuex——getters(变换state中的数据以提供访问)的基本使用

--- learnVuex/src/App.vue
+++ learnVuex/src/App.vue
@@ -5,6 +5,12 @@
     <button @click="addition">+</button>
     <button @click="subtraction">-</button>
 
+    <h2>---------------App内容:getter--------------</h2>
+    <h2>{{ $store.getters.powerCounter }}</h2>
+    <h2>年龄大于20的学生：{{$store.getters.more20Stu}}</h2>
+    <h2>个数：{{$store.getters.more20StuLength}}</h2>
+    <h2>年龄大于传入年龄的学生：{{$store.getters.moreAgeStu(25)}}</h2>
+
     <h2>---------------HelloVuex内容--------------</h2>
     <hello-vuex/>
   </div>
@@ -22,6 +28,11 @@ export default {
     return {
       message: '我是App组件'
     }
+  },
+  computed: {
+    // more20Stu() {
+    //   return this.$store.state.students.filter(s => {return s.age >= 20})
+    // }
   },
   methods :{
     addition() {
--- learnVuex/src/store/index.js
+++ learnVuex/src/store/index.js
@@ -6,7 +6,13 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   // 保存状态
   state: {
-    counter: 1000
+    counter: 1000,
+    students: [
+      {id: 110, name: 'why', age: 18},
+      {id: 111, name: 'kobe', age: 20},
+      {id: 112, name: 'bob', age: 25},
+      {id: 113, name: 'james', age: 31},
+    ]
   },
   // 建议通过mutations来修改state，而不是直接修改
   mutations: {
@@ -18,7 +24,29 @@ const store = new Vuex.Store({
     }
   },
   actions: {},
-  getters: {},
+
+  // 将state中数据变换后的数据
+  getters: {
+    powerCounter(state) {
+      return state.counter * state.counter
+    },
+    more20Stu(state) {
+      return state.students.filter(s => {
+        return s.age > 20
+      })
+    },
+    more20StuLength(state, getters) {
+      return getters.more20Stu.length
+    },
+    moreAgeStu(state) {
+      // return function (age) {
+      //   return state.students.filter(s => s.age > age)
+      // }
+      return age => {
+        return state.students.filter(s => s.age > age)
+      }
+    }
+  },
   modules: {}
 })
 
